5bee873521a4780cdb8856d12b153594e748e236
renanz <renan_zelaya@hotmail.com>
2018-10-22 21:05:41 -0600

    Github Api

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 var http = require("http");
-var url = require('url');
-var request = require('request');
+var url = require("url");
+var request = require("request");
 
 options = {
-    url: "https://api.github.com/users/:user",
-    headers: {
-        "User-Agent": "renanz"
-    }
-}
+  url: "https://api.github.com/users/renanz",
+  headers: {
+    "User-Agent": "renanz"
+  }
+};
 
-request.get(options).on('response', function(response) {
-    console.log(response.statusCode);
-    console.log(JSON.stringify(response));
-})
+txt = "";
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.write(txt);
-    res.end();
-  }).listen(8080);
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    request.get(options).on("response", function(response) {
+      res.write(JSON.stringify(response));
+      res.end();
+    });
+  })
+  .listen(8080);
